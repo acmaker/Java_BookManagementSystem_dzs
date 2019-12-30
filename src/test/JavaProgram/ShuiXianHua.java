191ba8be07caba928e7b8409c0676e830a41200b
acmaker <57260481+acmaker@users.noreply.github.com>
2019-12-30 12:06:52 +0800

    modify ShuiXianHua, modify error about mod.

--- src/test/JavaProgram/ShuiXianHua.java
+++ src/test/JavaProgram/ShuiXianHua.java
@@ -7,7 +7,7 @@ public class ShuiXianHua {
         int counter = 0;
         for ( int i = 100; i <= 999; ++i ) {
             int a = i/100;
-            int b = i%100/10;
+            int b = i/10%10;
             int c = i%10;
             if ( Math.pow( a, 3) + Math.pow( b, 3 ) + Math.pow( c, 3 ) == i ) {
                 counter++;
